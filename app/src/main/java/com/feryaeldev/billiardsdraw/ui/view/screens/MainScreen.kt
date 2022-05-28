d7094f4c582a1984b2a9e0ebe4db9649cebe2e7f
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2022-05-28 21:09:21 +0200

    almost v1

--- app/src/main/java/com/feryaeldev/billiardsdraw/ui/view/screens/MainScreen.kt
+++ app/src/main/java/com/feryaeldev/billiardsdraw/ui/view/screens/MainScreen.kt
@@ -1,12 +1,46 @@
 package com.feryaeldev.billiardsdraw.ui.view.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.feryaeldev.billiardsdraw.R
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    Text(text = stringResource(id = R.string.app_name))
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.banner),
+                contentDescription = "Banner",
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(100.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(name = "Main Preview")
+@Composable
+fun MainScreenPreview() {
+    MainScreen(navController = rememberNavController())
 }
