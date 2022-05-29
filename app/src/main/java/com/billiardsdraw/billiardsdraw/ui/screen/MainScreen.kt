35215cb51ae4a7fafa1a95c85d3ca7c477ffb835
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2022-05-29 19:03:57 +0200

    changes

--- app/src/main/java/com/billiardsdraw/billiardsdraw/ui/screen/MainScreen.kt
+++ app/src/main/java/com/billiardsdraw/billiardsdraw/ui/screen/MainScreen.kt
@@ -1,14 +1,31 @@
 package com.billiardsdraw.billiardsdraw.ui.screen
 
+import android.widget.Space
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*;
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.TextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,25 +35,93 @@ import com.billiardsdraw.billiardsdraw.R
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Card(elevation = 4.dp, modifier = Modifier.fillMaxSize()) {
             Image(
-                painter = painterResource(id = R.drawable.banner),
-                contentDescription = "Banner",
+                painter = painterResource(id = R.drawable.fondoinicio),
+                contentDescription = "Fondo inicio",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
-                    .height(200.dp)
-            )
-            Text(
-                text = stringResource(id = R.string.welcome) + " " + stringResource(id = R.string.app_name),
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold
-            )
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "Logo",
+                        modifier = Modifier
+                            .width(150.dp)
+                            .height(150.dp)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.welcome) + " " + stringResource(id = R.string.app_name),
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Spacer(modifier = Modifier.height(40.dp))
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Enter email", color = Color.Black) },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        modifier = Modifier.background(
+                            Color.White
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Enter password", color = Color.Black) },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        modifier = Modifier.background(Color.White)
+                    )
+                    Spacer(modifier = Modifier.height(1.dp))
+                    Row() {
+                        Checkbox(checked = true, onCheckedChange = {})
+                        Text(
+                            text = "Mantener sesión iniciada",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(1.dp))
+                    Text(
+                        text = "¿Has olvidado tu contraseña?",
+                        Modifier
+                            .clickable { }
+                            .fillMaxWidth()
+                            .align(Alignment.Start)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "¿Acabas de llegar?", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.width(120.dp)) {
+                        Text(
+                            text = "Únete ya"
+                        )
+                    }
+                }
+            }
         }
     }
 }
