5177f2a86b3c1b0aeea6d92dbb8e4b2b2168de7b
FeryaelJustice <nano9gs@hotmail.es>
2022-06-26 14:19:07 +0200

    feat(src): add build variants and flavors

---
+++ app/src/main/java/com/billiardsdraw/billiardsdraw/ui/util/AppUtils.kt
@@ -0,0 +1,21 @@
+package com.billiardsdraw.billiardsdraw.ui.util
+
+/*
+fun getBuildConfig(){
+    when (BuildConfig.BUILD_TYPE) {
+        "debug" -> {
+            println("debug")
+            sentence += "debug"
+        }
+        "release" -> {
+            print("release")
+            // Inicializar Admob
+            sentence += "release"
+        }
+        else -> {
+            print("")
+            sentence += " "
+        }
+    }
+}
+*/
